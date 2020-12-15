ca9a7128fab6b5f8e51ec2fee73a3d27e99425e8
Víctor Piñón <victorpinonrattia@hotmail.com>
2020-12-15 20:26:52 +0100

    ex 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,17 @@ const totalLikes = (blogList) => {
     return blogList.reduce((sum, curr) => (sum + curr.likes), 0)
 }
 
+const favoriteBlog = (blogList) => {
+    const favoriteBlog = blogList.reduce((fav, curr) => (fav.likes < curr.likes ? curr : fav))
+    return {
+        title: favoriteBlog.title,
+        author: favoriteBlog.author,
+        likes: favoriteBlog.likes
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
