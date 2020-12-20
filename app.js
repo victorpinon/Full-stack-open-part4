a7ecb3d6c530692b2fab958c105ecd5be7f5a7dc
Víctor Piñón <victorpinonrattia@hotmail.com>
2020-12-20 21:40:55 +0100

    ex 4.18

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('./controllers/blogs.js')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware.js')
 const logger = require('./utils/logger.js')
 const config = require('./utils/config.js')
@@ -26,6 +27,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
