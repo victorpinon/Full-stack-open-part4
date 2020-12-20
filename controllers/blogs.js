97d7512cff65d20f37d4df48f0c9b89a9b06a6fd
Víctor Piñón <victorpinonrattia@hotmail.com>
2020-12-20 21:53:17 +0100

    ex 4.20

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,14 +6,6 @@ require('express-async-errors')
 
 const blogsRouter = express.Router()
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
-
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
         .populate('user', { username: 1, name: 1 })
@@ -27,9 +19,9 @@ blogsRouter.post('/', async (request, response) => {
     else {
         const body = request.body
 
-        const token = getTokenFrom(request)
-        const decodedToken = jwt.verify(token, process.env.SECRET)
-        if (!token || !decodedToken.id) {
+        // const token = getTokenFrom(request)
+        const decodedToken = jwt.verify(request.token, process.env.SECRET)
+        if (!request.token || !decodedToken.id) {
             return response.status(401).json({ error: 'token missing or invalid' })
         }
         const user = await User.findById(decodedToken.id)
