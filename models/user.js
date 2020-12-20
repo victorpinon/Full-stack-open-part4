cf15e3f96aa50036421cbef9bed50e8e4342c333
Víctor Piñón <victorpinonrattia@hotmail.com>
2020-12-20 12:52:07 +0100

    ex 4.15

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+// const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = mongoose.Schema({
+    username: String,
+    name: String,
+    passwordHash: String
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+    }
+})
+
+// userSchema.plugin(uniqueValidator)
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,8 +3,8 @@ const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app')
 const Blog = require('../models/blog')
-
-
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
 beforeEach(async () => {
     await Blog.deleteMany({})
@@ -149,6 +149,39 @@ describe('updating a blog', () => {
             ...blogToUpdate,
             likes: 99
         })
+    })
+})
+
+describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret', 10)
+        const user = new User({ username: 'root', passwordHash })
+
+        await user.save()
+    })
+
+    test('creation succeeds with a fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'mluukkai',
+            name: 'Matti Luukkainen',
+            password: 'salainen',
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
     })
 })
 
